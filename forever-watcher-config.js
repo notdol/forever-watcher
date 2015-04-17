168341c573941c23f6cccc950fba8210095519d7
notdol <notdol@puseoks-Mac-mini-2.local>
2015-04-17 13:08:10 +0900

    first Commit

---
+++ forever-watcher-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	error : [],
+	start : [],
+	stop : [],
+	restart : [
+		{type: 'http', domain: 'kato', options: { url : "https://api.kato.im/rooms/e141542d9fbef568d59c57239cfaf7caa307301abd04730b62d7390c49a8e38/simple" } }
+	],
+	exit : [],
+	stdout : [],
+	stderr : []
+}
